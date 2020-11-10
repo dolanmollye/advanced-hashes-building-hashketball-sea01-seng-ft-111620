3ad23f06cafdd41366f288dec32d9c5fcbb0f1dd
Molly Dolan <dolanmollye@gmail.com>
2020-11-10 05:56:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash 
   {
   :home => {
-    :team_name => {"Brooklyn Nets"},
+    :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
     :players => [
       {
@@ -65,7 +65,7 @@ def game_hash
   
   
   :away => {
-    :team_name => {"Charlotte Hornets"},
+    :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
     :players => [
       {
